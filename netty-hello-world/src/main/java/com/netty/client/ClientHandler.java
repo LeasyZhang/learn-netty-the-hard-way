ad072c242f9bde501c3e244d61212d4b4d8d2733
joe zhang <joe.zhang@ringcentral.com>
2020-01-29 15:25:01 +0800

    helloworld

---
+++ netty-hello-world/src/main/java/com/netty/client/ClientHandler.java
@@ -0,0 +1,20 @@
+package com.netty.client;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+@ChannelHandler.Sharable
+public class ClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void messageReceived(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.err.println(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
